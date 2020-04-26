61749b36f01347a4e064e07d06a7745a867da276
dalignyclem@gmail.com <dalignyclem@gmail.com>
2020-04-26 11:25:30 +0200

    Getting used to React + starting the home page

--- src/components/pages/home/HomePage.js
+++ src/components/pages/home/HomePage.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './HomePage.scss';
-import { SubTitle, Title } from '../../common/text/Basics';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-const PlaceHolder = ({ style }) => (
-  <div className='placeholder_box' style={style}>
+const Campus = ({ style }) => (
+  <div className='campus_box' style={style}>
     test
   </div>
 );
@@ -15,27 +14,14 @@ const PlaceHolder = ({ style }) => (
 export function HomePage() {
   return (
     <div>
-      <Title>HomePage</Title>
-      <SubTitle>Lorem ipsum dolor sit amet</SubTitle>
       <Container fluid>
         <Row fluid>
           {/* A ROW IS 12  sm is small width, md: is long width*/}
-          <Col sm='12' md='4'>
-            <PlaceHolder style={{ backgroundColor: '#F9F9F9' }} />
-          </Col>
-          <Col sm='12' md='8'>
-            <PlaceHolder style={{ backgroundColor: 'rgba(195,126,10,0.21)' }} />
-          </Col>
-        </Row>
-        <Row className='justify-content-md-center'>
           <Col sm='12' md='6'>
-            <PlaceHolder style={{ backgroundColor: '#BDBDBD' }} />
+            <Campus style={{ backgroundColor: '#F9F9F9' }} />
           </Col>
-          <Col sm='12'>
-            <PlaceHolder style={{ backgroundColor: '#F9F9F9' }} />
-          </Col>
-          <Col sm='12'>
-            <PlaceHolder style={{ backgroundColor: 'rgba(3,169,244,0.25)' }} />
+          <Col sm='12' md='6'>
+            <Campus style={{ backgroundColor: 'rgba(195,126,10,0.21)' }} />
           </Col>
         </Row>
       </Container>
