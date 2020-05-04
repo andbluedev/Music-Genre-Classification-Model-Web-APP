f00841f5879e410e8a53abc134e697867f94f01a
dalignyclem@gmail.com <dalignyclem@gmail.com>
2020-05-04 23:14:47 +0200

    Correction erreurs

--- src/components/pages/Home tech/HomeTechPage.js
+++ src/components/pages/Home tech/HomeTechPage.js
@@ -7,36 +7,37 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
 export function HomeTechPage() {
-    const DeviceType = ({name}) => {
-        return (
-            <Card style={{width: '8rem'}}>
-                <Card.Body>
-                    <Card.Title> X </Card.Title>
-                    <Card.Text> {name} </Card.Text>
-                </Card.Body>
-            </Card>
-        );
-    };
-
-    const BdButton = ({descr}) => {
-        return (
-            <button className="button">
-                <p className='descrButton'> {descr} </p>
-            </button>
-        );
-    };
-    const Breackdown = ({device}, {type}, {date}, {state}) => {
+  const DeviceType = ({ name }) => {
     return (
+      <Card style={{ width: '8rem' }}>
+        <Card.Body>
+          <Card.Title> X </Card.Title>
+          <Card.Text> {name} </Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  };
+  const BdButton = ({ descr }) => {
+    return (
+      <Button className='button'>
+        <p className='descrButton'> {descr} </p>
+      </Button>
+    );
+  };
+  const Breackdown = ({ device }, { type }, { date }, { state }) => {
+    return (
+      <div>
         <p> Appareil : {device} </p>
         <p> Type de panne : {type} </p>
         <p> Date : {date} </p>
         <p> Etat : {state} </p>
-
-        <BdButton descr="Voir description"/>
-        <BdButton descr="Résoudre"/>
-            <BdButton descr="Voir Salle"/>
+        <BdButton descr='Voir description' />
+        <BdButton descr='Résoudre' />
+        <BdButton descr='Voir Salle' />
+      </div>
     );
-};
+  };
+  return (
     <div>
       <Container fluid>
         <Row fluid>
@@ -49,6 +50,36 @@ export function HomeTechPage() {
         </Row>
         <Row fluid>
           <Title>Liste des pannes</Title>
+          <Breackdown
+            device='Projecteur'
+            type='ampoule cassée'
+            date='14/04/2020'
+            state='En réparation'
+          />
+          <Breackdown
+            device='Projecteur'
+            type='ampoule cassée'
+            date='14/04/2020'
+            state='En réparation'
+          />
+          <Breackdown
+            device='Projecteur'
+            type='ampoule cassée'
+            date='14/04/2020'
+            state='En réparation'
+          />
+          <Breackdown
+            device='Projecteur'
+            type='ampoule cassée'
+            date='14/04/2020'
+            state='En réparation'
+          />
+          <Breackdown
+            device='Projecteur'
+            type='ampoule cassée'
+            date='14/04/2020'
+            state='En réparation'
+          />
         </Row>
       </Container>
     </div>
