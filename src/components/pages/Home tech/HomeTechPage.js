65941a99d256bc6769b1d021cd98ec1974add788
dalignyclem@gmail.com <dalignyclem@gmail.com>
2020-05-04 23:00:59 +0200

    Avancées

--- src/components/pages/Home tech/HomeTechPage.js
+++ src/components/pages/Home tech/HomeTechPage.js
@@ -3,18 +3,47 @@ import './HomeTechPage.scss';
 import { Title } from '../../common/text/Basics';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-
-function DeviceType({ name }) {
-  return <div> {name}</div>;
-}
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 export function HomeTechPage() {
-  return (
+    const DeviceType = ({name}) => {
+        return (
+            <Card style={{width: '8rem'}}>
+                <Card.Body>
+                    <Card.Title> X </Card.Title>
+                    <Card.Text> {name} </Card.Text>
+                </Card.Body>
+            </Card>
+        );
+    };
+
+    const BdButton = ({descr}) => {
+        return (
+            <button className="button">
+                <p className='descrButton'> {descr} </p>
+            </button>
+        );
+    };
+    const Breackdown = ({device}, {type}, {date}, {state}) => {
+    return (
+        <BdButton descr="Voir description"/>
+        <BdButton descr="Résoudre"/>
+            <BdButton descr="Voir Salle"/>
+    );
+};
     <div>
       <Container fluid>
         <Row fluid>
           <Title>Liste des types d'appareil</Title>
-          <DeviceType name='1' />
+          <DeviceType name='Ampoule cassée' />
+          <DeviceType name='Ampoule cassée' />
+          <DeviceType name='Ampoule cassée' />
+          <DeviceType name='Ampoule cassée' />
+          <DeviceType name='Ampoule cassée' />
+        </Row>
+        <Row fluid>
+          <Title>Liste des pannes</Title>
         </Row>
       </Container>
     </div>
