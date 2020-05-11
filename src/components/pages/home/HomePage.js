f07692d323c5d937ec95d92e741c56c60e3c6e3f
Laurianne <laurianne.progent@isep.fr>
2020-05-11 22:16:28 +0200

    fetch buildings in home + added links to building pages

--- src/components/pages/home/HomePage.js
+++ src/components/pages/home/HomePage.js
@@ -1,29 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './HomePage.scss';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { get } from '../../../data/api';
 
-function CampusButton({ campusName }) {
+function CampusButton({ campusName, href }) {
   return (
-    <button className='campus'>
-      <p className='name'> {campusName}</p>
-    </button>
+    <a href={href}>
+      <button className='campus'>
+        <p className='name'> {campusName}</p>
+      </button>
+    </a>
   );
 }
 
 export function HomePage() {
+  const [buildings, setBuildings] = useState([]);
+
+  useEffect(() => {
+    get('/buildings').then((result) => {
+      setBuildings(result.payload);
+    });
+  }, []);
+
+  const allCampusButton = [];
+  buildings.forEach((building) =>
+    allCampusButton.push(
+      <Col sm='12' md='6'>
+        <CampusButton campusName={building.name} href={'/building/' + building.id} />
+      </Col>
+    )
+  );
+
   return (
     <div>
       <Container fluid>
-        <Row fluid>
-          <Col sm='12' md='6'>
-            <CampusButton campusName='NDC' href='#' />
-          </Col>
-          <Col sm='12' md='6'>
-            <CampusButton campusName='NDL' href='#' />
-          </Col>
-        </Row>
+        <Row fluid>{allCampusButton}</Row>
       </Container>
     </div>
   );
