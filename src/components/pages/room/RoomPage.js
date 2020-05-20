5edba19d6a589a6cedb0688f7ebf760e8673a597
LeopoldBancal <leopold.bancal@free.fr>
2020-05-20 14:53:36 +0200

    bbbb

--- src/components/pages/room/RoomPage.js
+++ src/components/pages/room/RoomPage.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { FailureDisplay } from './failureSection/FailureDisplay';
+import { FailureDisplay } from './failureSection/failureDisplay';
 import './RoomPage.scss';
 import { SubTitle, Title } from '../../common/text/Basics';
 import { useParams } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import { get } from '../../../data/api';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import { FailureTypeManagement } from './failureTypeSection/FailureTypeManagement';
 
 export function RoomPage() {
   let { id } = useParams();
@@ -25,17 +28,23 @@ export function RoomPage() {
       <Title> {room.number} </Title>
       <SubTitle> Batiment : {building.name}</SubTitle>
       <Container className='room-wrapper'>
-        {failures.map((failure) => (
-          <FailureDisplay
-            id={failure.id}
-            type={failure.title}
-            device={failure.deviceCategory}
-            date={failure.createdAt}
-            description={failure.description}
-            state={failure.state}
-            upvoters={failure.upvoters}
-          />
-        ))}
+        <Row>
+          <Col>
+            {failures.map((failure) => (
+              <FailureDisplay
+                type={failure.title}
+                device={failure.deviceCategory}
+                date={failure.createdAt}
+                description={failure.description}
+                state={failure.state}
+              />
+            ))}
+          </Col>
+        </Row>
+      </Container>
+      <SubTitle>Gestion de la salle</SubTitle>
+      <Container>
+        <FailureTypeManagement roomId={id} />
       </Container>
     </div>
   );
