bc7522909f01cdcca7cd34dd05185473488656a4
npasquie <nicolas.pasquier.pro@gmail.com>
2020-05-23 19:41:09 +0200

    new component SelectionsInForm

--- src/components/layout/navbar/FormReportBreakdown/FormReportBreakdown.js
+++ src/components/layout/navbar/FormReportBreakdown/FormReportBreakdown.js
@@ -2,45 +2,12 @@ import React, { useState } from 'react';
 import { Modal, Form, Button } from 'react-bootstrap';
 import { post } from '../../../../data/api';
 import { useFormState } from '../formContext/FormContext';
-import { FormActionType } from '../formContext/FormReducer';
-import { FormInput } from './FormInput/FormInput';
+import { SelectionsInForm } from './SelectionsInForm/SelectionsInForm';
 
 export function FormReportBreakdown(props) {
   const formState = useFormState();
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
-
-  function renderSelections() {
-    let selectFormPart = [
-      <FormInput
-        type={FormActionType.CHOOSE_BUILDING}
-        fetchURL={'buildings'}
-        label={'Bâtiment'}
-        apiKey={'name'}
-      />
-    ];
-    if (formState.selectedBuildingId) {
-      selectFormPart.push(
-        <FormInput
-          type={FormActionType.CHOOSE_ROOM}
-          fetchURL={`buildings/${formState.selectedBuildingId}/rooms`}
-          label={'Salle'}
-          apiKey={'number'}
-        />
-      );
-    }
-    if (formState.selectedRoomId) {
-      selectFormPart.push(
-        <FormInput
-          type={FormActionType.CHOOSE_DEVICE}
-          fetchURL={`rooms/${formState.selectedRoomId}/device/categories`}
-          label={'Appareil'}
-          apiKey={'name'}
-        />
-      );
-    }
-    return selectFormPart;
-  }
 
   function handleSubmit() {
     post(
@@ -74,7 +41,7 @@ export function FormReportBreakdown(props) {
               Veuillez donner un titre à cette panne.
             </Form.Control.Feedback>
           </Form.Group>
-          {renderSelections()}
+          <SelectionsInForm />
           <Form.Group>
             <Form.Label>Description :</Form.Label>
             <Form.Control
---
+++ src/components/layout/navbar/FormReportBreakdown/SelectionsInForm/SelectionsInForm.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useFormState } from '../../formContext/FormContext';
+import { FormInput } from '../FormInput/FormInput';
+import { FormActionType } from '../../formContext/FormReducer';
+
+export function SelectionsInForm() {
+  const formState = useFormState();
+
+  let toReturn = [
+    <FormInput
+      type={FormActionType.CHOOSE_BUILDING}
+      fetchURL={'buildings'}
+      label={'Bâtiment'}
+      apiKey={'name'}
+    />
+  ];
+  if (formState.selectedBuildingId) {
+    toReturn.push(
+      <FormInput
+        type={FormActionType.CHOOSE_ROOM}
+        fetchURL={`buildings/${formState.selectedBuildingId}/rooms`}
+        label={'Salle'}
+        apiKey={'number'}
+      />
+    );
+  }
+  if (formState.selectedRoomId) {
+    toReturn.push(
+      <FormInput
+        type={FormActionType.CHOOSE_DEVICE}
+        fetchURL={`rooms/${formState.selectedRoomId}/device/categories`}
+        label={'Appareil'}
+        apiKey={'name'}
+      />
+    );
+  }
+  return toReturn;
+}
