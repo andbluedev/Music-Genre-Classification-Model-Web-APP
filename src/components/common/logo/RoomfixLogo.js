6f5cd295e49746174db1e8aeae03f58620ab6922
Andrew Pouret <apouret@juniorisep.com>
2020-05-25 15:21:24 +0200

    fix logo width stretch

--- src/components/common/logo/RoomfixLogo.js
+++ src/components/common/logo/RoomfixLogo.js
@@ -1,6 +1,9 @@
-import logoSvg from '../../../img/roomfix_light_325x325.png';
 import React from 'react';
+import './RoomfixLogo.scss';
+import logoSvg from '../../../img/roomfix_light_325x325.png';
 
 export const RoomfixLogo = (props) => (
-  <img src={logoSvg} alt='react-logo' className={props.className} />
+  <section className='logo-wrapper'>
+    <img src={logoSvg} alt='react-logo' className={props.className} />
+  </section>
 );
