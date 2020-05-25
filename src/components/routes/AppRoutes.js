9f0c9c06be4e58d79b3186ec5a13a72cf090acf3
Laurianne <laurianne.progent@isep.fr>
2020-05-25 22:56:20 +0200

    I'm trying

--- src/components/routes/AppRoutes.js
+++ src/components/routes/AppRoutes.js
@@ -1,40 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { HomePage } from '../pages/home/HomePage';
-import { AboutPage } from '../pages/about/AboutPage';
-import { FilePage } from '../pages/tp/TpPage';
-import { AdminPage } from '../pages/admin/AdminPage';
-import { BuildingPage } from '../pages/building/BuildingPage';
-import { RoomPage } from '../pages/room/RoomPage';
-import { UserPage } from '../pages/account/user/UserPage';
+import { AppPage } from '../pages/AppPage';
+
 export const AppRoutes = () => (
   <Switch>
-    <Route path='/about'>
-      <AboutPage />
-    </Route>
-
-    <Route path='/user'>
-      <UserPage />
-    </Route>
-
-    <Route path='/admin'>
-      <AdminPage />
-    </Route>
-
-    <Route exact path='/home'>
-      <HomePage />
-    </Route>
-
-    <Route path='/room/:id'>
-      <RoomPage />
-    </Route>
-
-    <Route path='/tp'>
-      <FilePage />
-    </Route>
-
-    <Route path='/building/:id'>
-      <BuildingPage />
-    </Route>
+    <Route path='/about' component={AppPage} />
+    <Route path='/user' component={AppPage} />
+    <Route path='/admin' component={AppPage} />
+    <Route exact path='/home' component={AppPage} />
+    <Route path='/room/:id' component={AppPage} />
+    <Route path='/tp' component={AppPage} />
+    <Route path='/building/:id' component={AppPage} />
   </Switch>
 );
