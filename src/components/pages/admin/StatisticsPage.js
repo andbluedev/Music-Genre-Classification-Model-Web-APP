1b71be967bfcd5fc5178b85332542b49f212b7a6
Andrew Pouret <apouret@juniorisep.com>
2020-05-26 12:03:16 +0200

    added role acess control to statistic page in admin backoffice

--- src/components/pages/admin/StatisticsPage.js
+++ src/components/pages/admin/StatisticsPage.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Title } from '../../common/text/Basics';
 import { BuildingSection } from './buildStatisticSection/BuildingSection';
+import { UserContext } from '../../../data/auth/UserContext';
 
 export function StatisticsPage() {
-  return (
+  const { state } = useContext(UserContext);
+  return state.role === 'ADMIN' ? (
     <div>
       <Title>Statistiques par Batiment</Title>
       <BuildingSection entityEndpointName='building' />
       <Title>Statistiques par Salles</Title>
       <BuildingSection entityEndpointName='room' />
     </div>
+  ) : (
+    <div> Vous n'avez pas le statut pour accéder à cette page </div>
   );
 }
